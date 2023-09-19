e73f836354b830c00a3c21971cab2b976abec952
B FAROOQ <bfarooq2022@gmail.com>
2023-09-19 19:31:36 +0530

    files addded

---
+++ script.js
@@ -0,0 +1,27 @@
+//select all required elements 
+
+// const play_pause = document.querySelector(".play");
+// const previous = document.querySelector(".previous-play");
+// const forward = document.querySelector(".forward-play");
+
+
+// let musicIndex = 2;
+
+// window.addEventListener("load",() =>{
+//     loadmusic(musicIndex);
+// });
+
+// function loadmusic(){
+//     play_pause.src = `${allMusic[musicIndex-1].src}.mp3`;
+// }
+
+// play_pause.addEventListener("click", () =>{
+    
+// });
+
+const jawan = document.querySelector(".jawan");
+
+function playmusic(){
+    jawan.play();
+
+}
